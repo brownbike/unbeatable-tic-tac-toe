2e24aa36dfc708cb1a1db1b3ca117ef4de674a96
Scott McKeon <scottmckeon@protonmail.com>
2025-08-13 16:49:42 -0700

    Add minimax func to replace best move logic

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,7 +4,6 @@ use axum::{
     routing::{get, put},
 };
 use log::info;
-use rand::seq::IndexedRandom;
 use serde::{Deserialize, Serialize};
 use tower_http::cors::{Any, CorsLayer};
 use tracing_subscriber::EnvFilter;
@@ -41,17 +40,16 @@ async fn root() -> &'static str {
     "Welcome to tic-tac-toe"
 }
 
-// NOTE: For simplicity sake, the human is always X and the computer is always O.
-// NOTE: I'd like to use &str, but ran into lifetime issues so, strings it is.
+// NOTE: Remember, the human is always X and the computer is always O.
 async fn calculate_move(Json(payload): Json<Game>) -> (StatusCode, Json<Game>) {
     let mut board: Vec<String> = payload.board;
     let mut status: Status = Status::InProgress;
 
-    if is_winner(&board, "x".to_string()) {
+    if is_winner(&board, "x") {
         // Human won :(
         info!("X wins, this shouldn't happen");
         status = Status::XWins;
-    } else if is_winner(&board, "0".to_string()) {
+    } else if is_winner(&board, "o") {
         // Computer won :)
         info!("O wins!! Computers rule, humans drool");
         status = Status::OWins;
@@ -59,13 +57,13 @@ async fn calculate_move(Json(payload): Json<Game>) -> (StatusCode, Json<Game>) {
         // A draw :|
         info!("It's a Draw");
         status = Status::Draw;
-    } else if let Some(space) = can_win(&board, "o".to_string()) {
-        // We can win! Take the square!
-        make_move(&mut board, space);
+    } else if let Some(winning_move) = can_win(&board, "o") {
+        // If we can win, take the square!
+        make_move(&mut board, winning_move, "o");
         status = Status::OWins;
-    } else if let Some(space) = best_move(&board) {
-        // make some moves
-        make_move(&mut board, space);
+    } else if let Some(best_move) = minimax_move(&board) {
+        // Use some fancy math to calculate the best move
+        make_move(&mut board, best_move, "o");
     }
 
     let new_turn = Game {
@@ -76,13 +74,13 @@ async fn calculate_move(Json(payload): Json<Game>) -> (StatusCode, Json<Game>) {
     (StatusCode::OK, Json(new_turn))
 }
 
-fn can_win(board: &Vec<String>, letter: String) -> Option<usize> {
+fn can_win(board: &Vec<String>, letter: &str) -> Option<usize> {
     let available_moves = possible_moves(&board, (0..9).collect());
 
     for space in available_moves {
         let mut test_board = board.clone();
-        test_board[space] = letter.clone();
-        if is_winner(&test_board, letter.clone()) {
+        make_move(&mut test_board, space, "o");
+        if is_winner(&test_board, letter) {
             info!("{letter} will win with {space}");
             return Some(space);
         }
@@ -92,111 +90,88 @@ fn can_win(board: &Vec<String>, letter: String) -> Option<usize> {
     None
 }
 
-fn can_fork(board: &Vec<String>, letter: String) -> Option<usize> {
-    let available_moves = possible_moves(&board, (0..9).collect());
+// Evaluate all possible moves and find the best one
+fn minimax(board: &Vec<String>, depth: i32, is_maximizing: bool) -> i32 {
+    // Can we win?
+    if is_winner(board, "o") {
+        return 10 - depth;
+    }
+
+    // Do the humans win?
+    if is_winner(board, "x") {
+        return depth - 10;
+    }
+
+    // Is it a draw?
+    if is_draw(board, (0..9).collect()) {
+        return 0;
+    }
+
+    let available_moves = possible_moves(board, (0..9).collect());
+
+    if is_maximizing {
+        // Score the computer moves
+        let mut best_score = i32::MIN;
+        for space in available_moves {
+            let mut test_board = board.clone();
+            make_move(&mut test_board, space, "o");
+            let score = minimax(&test_board, depth + 1, false);
+            best_score = best_score.max(score);
+        }
+        best_score
+    } else {
+        // Score the human moves
+        let mut best_score = i32::MAX;
+        for space in available_moves {
+            let mut test_board = board.clone();
+            make_move(&mut test_board, space, "x");
+            let score = minimax(&test_board, depth + 1, true);
+            best_score = best_score.min(score);
+        }
+        best_score
+    }
+}
+
+fn minimax_move(board: &Vec<String>) -> Option<usize> {
+    let available_moves = possible_moves(board, (0..9).collect());
+    // Start with the smallest int possible
+    let mut best_score = i32::MIN;
+    let mut best_move = None;
 
     for space in available_moves {
         let mut test_board = board.clone();
-        test_board[space] = letter.clone();
+        make_move(&mut test_board, space, "o");
+        // Score every possible move
+        let score = minimax(&test_board, 0, false);
 
-        let winning_positions = count_winning_positions(&test_board, letter.clone());
-        if winning_positions >= 2 {
-            info!("{letter} can fork at position {space} with {winning_positions} wins");
-            return Some(space);
+        // We found a better move
+        if score > best_score {
+            best_score = score;
+            best_move = Some(space);
         }
     }
 
-    None
+    if let Some(mv) = best_move {
+        info!("The best move {mv} scored {best_score}");
+    }
+    best_move
 }
 
-fn count_winning_positions(board: &Vec<String>, letter: String) -> usize {
-    let available_moves = possible_moves(&board, (0..9).collect());
-    let mut winning_count = 0;
-
-    for space in available_moves {
-        let mut test_board = board.clone();
-        test_board[space] = letter.clone();
-        if is_winner(&test_board, letter.clone()) {
-            winning_count += 1;
-        }
-    }
-
-    winning_count
+fn make_move(board: &mut Vec<String>, space: usize, letter: &str) {
+    board[space] = letter.to_string();
 }
 
-fn best_move(board: &Vec<String>) -> Option<usize> {
-    // If human can win on the next move, block them
-    if let Some(space) = can_win(&board, "x".to_string()) {
-        info!("Human can win, block them!");
-        return Some(space);
-    }
+fn is_winner(board: &Vec<String>, letter: &str) -> bool {
+    let l = letter.to_string();
 
-    // Opening moves
-    if is_first_human_move(&board) {
-        // If the human chooses the center square as their first move, pick a corner.
-        if !is_space_free(&board, 4) {
-            // Let's prioritize a corner space
-            info!("The human chose the center square first, play a corner");
-            return choose_corner(&board);
-        }
-
-        // The center is free, let's take it
-        if is_space_free(&board, 4) {
-            info!("play the center");
-            return Some(4);
-        }
-    }
-
-    // Try to create a fork
-    if let Some(space) = can_fork(&board, "o".to_string()) {
-        info!("Fork at position {space}");
-        return Some(space);
-    }
-
-    // Block human fork
-    if let Some(space) = can_fork(&board, "x".to_string()) {
-        info!("Blocking human fork at {space}");
-        return Some(space);
-    }
-
-    // Choose corner
-    if let Some(rand_move) = choose_corner(board) {
-        info!("Pick a corner {rand_move}");
-        return Some(rand_move);
-    }
-
-    // As a last step choose a side
-    let random_side_move: Option<usize> = choose_random_move(&board, vec![3, 7, 5, 1]);
-    info!("Pick a side {random_side_move:#?}");
-
-    random_side_move
-}
-
-fn is_first_human_move(board: &Vec<String>) -> bool {
-    possible_moves(&board, (0..9).collect()).len() == 8
-}
-
-fn choose_corner(board: &Vec<String>) -> Option<usize> {
-    if let Some(rand_move) = choose_random_move(&board, vec![0, 6, 2, 8]) {
-        info!("Pick a corner");
-        return Some(rand_move);
-    }
-    None
-}
-
-fn make_move(board: &mut Vec<String>, space: usize) {
-    board[space] = "o".to_string();
-}
-
-fn is_winner(board: &Vec<String>, letter: String) -> bool {
-    (board[0] == letter && board[1] == letter && board[2] == letter) // across the top
-    || (board[3] == letter && board[4] == letter && board[5] == letter) // across the middle
-    || (board[6] == letter && board[7] == letter && board[8] == letter) // across the bottom
-    || (board[0] == letter && board[3] == letter && board[6] == letter) // down the left
-    || (board[1] == letter && board[4] == letter && board[7] == letter) // down the middle
-    || (board[2] == letter && board[5] == letter && board[8] == letter) // down the right
-    || (board[0] == letter && board[4] == letter && board[8] == letter) // diagonal
-    || (board[6] == letter && board[4] == letter && board[2] == letter) // diagonal
+    return (board[0] == l && board[1] == l && board[2] == l) // across the top
+    || (board[3] == l && board[4] == l && board[5] == l) // across the middle
+    || (board[6] == l && board[7] == l && board[8] == l) // across the bottom
+    || (board[0] == l && board[3] == l && board[6] == l) // down the left
+    || (board[1] == l && board[4] == l && board[7] == l) // down the middle
+    || (board[2] == l && board[5] == l && board[8] == l) // down the right
+    || (board[0] == l && board[4] == l && board[8] == l) // diagonal
+    || (board[6] == l && board[4] == l && board[2] == l); // diagonal
 }
 
 fn is_space_free(board: &Vec<String>, space: usize) -> bool {
@@ -215,18 +190,6 @@ fn possible_moves(board: &Vec<String>, moves_list: Vec<usize>) -> Vec<usize> {
 
 fn is_draw(board: &Vec<String>, moves_list: Vec<usize>) -> bool {
     possible_moves(board, moves_list).len() == 0
-}
-
-fn choose_random_move(board: &Vec<String>, moves_list: Vec<usize>) -> Option<usize> {
-    let possible_moves = possible_moves(board, moves_list);
-
-    if possible_moves.len() != 0 {
-        let random_move = possible_moves.choose(&mut rand::rng());
-        info!("{random_move:?}");
-        return random_move.copied();
-    } else {
-        return None;
-    }
 }
 
 #[derive(Serialize, Deserialize)]
